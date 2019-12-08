3fbb3a13924b168200045e54dfe5c6a2c0a345b1
Jakub Warchoł <kubus.w@tlen.pl>
2019-12-08 22:23:10 +0100

    Closes #8 Added Swagger. Downgrade Spring version

--- src/main/java/pl/pwsztar/premedsys/PreMedSysController.java
+++ src/main/java/pl/pwsztar/premedsys/PreMedSysController.java
@@ -4,8 +4,9 @@ import lombok.AccessLevel;
 import lombok.experimental.FieldDefaults;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import pl.pwsztar.premedsys.domain.DiseaseFacade;
 import pl.pwsztar.premedsys.dto.DiseasesDto;
 import pl.pwsztar.premedsys.dto.PreMedicalResultsDto;
@@ -13,8 +14,9 @@ import pl.pwsztar.premedsys.dto.SymptomesDto;
 import pl.pwsztar.premedsys.utils.WebPageUtil;
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/")
+@Api(value="premedsys", description="API for premedical diseases recognition")
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class PreMedSysController {
 
@@ -25,21 +27,25 @@ public class PreMedSysController {
     this.diseaseFacade = diseaseFacade;
   }
 
-  @RequestMapping(value = "", produces = "text/html")
+  @GetMapping(value = "", produces = "text/html")
+  @ApiOperation(value = "Get main server page content.")
   @ResponseBody
   public String getDefaultPage() {
     return WebPageUtil.getPage();
   }
 
   @GetMapping("diseases")
+  @ApiOperation(value = "Get polish and latin name of all recognizing by system diseases.")
   @ResponseBody
   public ResponseEntity<List<DiseasesDto>> getAllDiseases() { return ResponseEntity.ok(diseaseFacade.getDiseases().toJavaList()); }
 
   @GetMapping("symptomes")
+  @ApiOperation(value = "Get all polish name of recognizing disease symptomes.")
   @ResponseBody
   public ResponseEntity<List<SymptomesDto>> getAllSymptomes() { return ResponseEntity.ok(diseaseFacade.getDiseasesSymptomes().toJavaList()); }
 
   @PostMapping("recognition")
+  @ApiOperation(value = "Get list of diseases name, probability (based on pointed symptomes) and premedical recommendation.")
   public ResponseEntity<List<PreMedicalResultsDto>> getDiseasesBySymptomes(@RequestBody List<String> symptomes) {
     List<PreMedicalResultsDto> results = diseaseFacade.getDiseasesAndRecommendationsBySymptomesName(symptomes).toJavaList();
     return ResponseEntity.ok(results);
